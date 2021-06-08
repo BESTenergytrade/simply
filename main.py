21df5b8afca6511d4f951f14776dc549de66b227
jtm91 <jtm91@mailbox.tu-berlin.de>
2021-06-08 21:58:25 +0200

    Change example run printlines and make black

--- main.py
+++ main.py
@@ -17,11 +17,15 @@ if __name__ == "__main__":
             m.accept_order(order, a.receive_market_results)
 
         m.print()
-        # To run matching without effective clearing
-        m.match(show=True)
 
         m.clear()
         print("Matches of bid/ask ids: {}".format(m.get_all_matches()))
 
-        print("Check traded energy volume and price")
-        print(pd.DataFrame.from_dict([a.traded for a in sc.actors]).unstack().apply(pd.Series))
+        print("\nCheck traded energy volume and price at actor level")
+        print(
+            pd.DataFrame.from_dict([a.traded for a in sc.actors])
+            .unstack()
+            .apply(pd.Series)
+        )
+        print("\nCheck traded energy volume and price at market level")
+        print(m.trades)
