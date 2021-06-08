86e23904fae5d91e7de233a224d68dd92797b8a0
jtm91 <jtm91@mailbox.tu-berlin.de>
2021-06-08 07:59:06 +0200

    Add basic run multi timestep example of actors randomly bidding on a market that is cleared

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
+import pandas as pd
+
 from simply import scenario
-from simply.scenario import Scenario
+from simply.market import Market
+
+timesteps = 2
 
 if __name__ == "__main__":
     sc = scenario.create_random(12, 10)
     print(sc.to_dict())
 
-    sc.power_network.update_shortest_paths()
-    print(sc.power_network.short_paths)
+    for t in range(timesteps):
+        m = Market(t)
+        for a in sc.actors:
+            # TODO concurrent bidding of actors
+            # TODO integrate order type into order
+            order, o_type = a.generate_order()
+            if o_type == -1:
+                m.accept_bid(order, a.receive_market_results)
+            elif o_type == 1:
+                m.accept_ask(order, a.receive_market_results)
+            else:
+                raise ValueError
 
-    sc.power_network.plot()
-    sc.power_network.to_image(10, 7)
-    sc.power_network.to_json()
-    print(sc)
+        m.print()
+        # To run matching without effective clearing
+        m.match(show=True)
+
+        m.clear()
+        print("Matches of bid/ask ids: {}".format(m.get_all_matches()))
+
+        print("Check traded energy volume and price")
+        print(pd.DataFrame.from_dict([a.traded for a in sc.actors]).unstack().apply(pd.Series))
