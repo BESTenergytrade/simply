cc5449f6b8fedea08f7050d80a50f9934efde4f0
jtm91 <jtm91@mailbox.tu-berlin.de>
2021-06-08 07:45:06 +0200

    Let actors generate Bids and notify actor if market is cleared Actors: - Add PV timeseries - Adapt schedule, now it can be pos. or negative - rename generate bid to order, replacement still missing full consistency - Order is returned with absolute volume, and the sign, which is temporary solution - change traded list, to dictionary with time keys Market: - add current timestep - add order books as dataframe - add callback function list of accepted actors - aggregate splitted bids of single energy units and callback each accepted actor with market time, volume, price

--- simply/actor.py
+++ simply/actor.py
@@ -3,35 +3,44 @@ import pandas as pd
 
 
 class Actor:
-    def __init__(self, actor_id, df):
+    # TODO rename bids/asks to order and use named tuple
+    def __init__(self, actor_id, df, ls=1, ps=1):
         # TODO add battery component
         # TODO add individual scale values and scale time series accordingly
         self.id = actor_id
         self.t = 0
+        self.load_scale = ls
+        self.pv_scale = ps
         self.battery = None
-        self.load = df["load"].to_list()
-        self.schedule = df["load"].to_list()
+        self.load = ls * df["load"].to_list()
+        self.pv = ps * df["pv"].to_list()
         self.prices = df["prices"].to_list()
+        # perfect foresight
+        self.schedule = df["pv"] - df["load"]
         self.bids = []
-        self.traded = []
+        self.traded = {}
 
-    def generate_bid(self):
+    def generate_order(self):
         # TODO calculate amount of energy to fulfil personal schedule
         energy = self.schedule[self.t]
         # TODO simulate strategy: manipulation, etc.
         price = self.prices[self.t]
         # TODO take flexibility into account to generate the bid
 
-        new_bid = (self.t, self.id, energy, price)
+        new_bid = (self.t, self.id, abs(energy), price)
         # TODO place bid on market
         self.bids.append(new_bid)
         self.t += 1
 
-    def receive_market_results(self, energy, price):
+        return new_bid, np.sign(energy)
+
+
+    def receive_market_results(self, time, energy, price):
         # TODO update schedule, if possible e.g. battery
         # TODO post settlement of differences
+        assert time < self.t
         post = (energy, price)
-        self.traded.append(post)
+        self.traded[time] = post
 
     def to_dict(self):
         return self.id
@@ -42,8 +51,8 @@ def create_random(actor_id):
     # TODO add random generation signal (e.g. with PV characteristic)
     nb_ts = 24
     time_idx = pd.date_range("2021-01-01", freq="H", periods=nb_ts)
-    values = np.random.rand(nb_ts, 2)
-    cols = ["load", "prices"]
+    cols = ["load", "pv", "prices"]
+    values = np.random.rand(nb_ts, len(cols))
     df = pd.DataFrame(values, columns=cols, index=time_idx)
 
     return Actor(actor_id, df)
--- simply/market.py
+++ simply/market.py
@@ -3,11 +3,16 @@ import random
 
 
 class Market:
-    def __init__(self):
+    def __init__(self, time):
         # TODO tbd if lists or dicts or ... is used
+        self.t = time
         self.asks = []
         self.bids = []
+        self.asks_df = None
+        self.bids_df = None
         self.matches = {}
+        self.energy_unit = 0.1
+        self.actor_callback = {}
         self.seed = 42
 
     def get_bids(self):
@@ -23,49 +28,65 @@ class Market:
         print(self.get_bids())
         print(self.get_asks())
 
-    def accept_bid(self, bid):
+    def accept_bid(self, bid, callback):
         """
         :param bid: tuple
+        :param callback: callback function
         :return:
         """
+        assert bid[0] == self.t
         self.bids.append(bid)
+        self.actor_callback[bid[1]] = callback
 
-    def accept_ask(self, ask):
+    def accept_ask(self, ask, callback):
         """
         :param ask: tuple
+        :param callback: callback function
         :return:
         """
+        assert ask[0] == self.t
         self.asks.append(ask)
+        self.actor_callback[ask[1]] = callback
 
     def clear(self):
         # TODO match bids
         self.matches = self.match()
-        # TODO send notification to actors about cleared bids for further processing
+        # Send cleared bids and asks to actors for further processing via callback
+        for a_id, ac in self.actor_callback.items():
+            # TODO simplify with one order book
+            energy = self.bids_df[self.bids_df["actor_id"] == a_id].count()[0] * self.energy_unit
+            price = self.bids_df[self.bids_df["actor_id"] == a_id]["price"].mean() * self.energy_unit
+            if energy and price:
+                ac(self.t, energy, price)
+            energy = - self.asks_df[self.asks_df["actor_id"] == a_id].count()[0] * self.energy_unit
+            price = self.asks_df[self.asks_df["actor_id"] == a_id]["price"].mean() * self.energy_unit
+            if energy and price:
+                ac(self.t, energy, price)
 
     def match(self, show=False):
-        random.seed(42)
+        random.seed(self.seed)
         # TODO default match can be replaced in different subclass
         bids = self.get_bids()
         asks = self.get_asks()
         # Expand bids/asks to fixed energy quantity bids/asks with individual ids
-        bids = (
-            bids.reindex(bids.index.repeat(bids["energy"]))
+        self.bids_df = (
+            bids.reindex(bids.index.repeat(bids["energy"] / self.energy_unit))
             .drop("energy", axis=1)
             .reset_index()
         )
-        asks = (
-            asks.reindex(asks.index.repeat(asks["energy"]))
+        self.asks_df = (
+            asks.reindex(asks.index.repeat(asks["energy"] / self.energy_unit))
             .drop("energy", axis=1)
             .reset_index()
         )
-        bid_ids = list(bids.index)
-        ask_ids = list(asks.index)
+        bid_ids = list(self.bids_df.index)
+        ask_ids = list(self.asks_df.index)
         if show:
-            print(bids)
-            print(asks)
+            print(self.bids_df)
+            print(self.asks_df)
 
         # Do the actual matching
-        bid_ids = random.sample(bid_ids, len(ask_ids))
+        bid_ids = random.sample(bid_ids, min(len(ask_ids), len(bid_ids)))
         matches = {b: a for b, a in zip(bid_ids, ask_ids)}
 
         return matches
