d162523a54beee8a35a44973dfd1caeacd6acd7b
Daniel <daniel.busch@rl-institut.de>
2021-08-05 15:32:53 +0200

    add line to launch function in scenario.py

--- generate_scenario_example.py
+++ generate_scenario_example.py
@@ -2,7 +2,8 @@ from simply import scenario
 from simply.scenario import Scenario
 
 if __name__ == "__main__":
-    sc = scenario.create_random(12, 10)
+    # sc = scenario.create_random(12, 10)
+    sc = scenario.create_households_from_csv('C:\\Users\daniel.busch\simply\simply\data\households', 12, 10)
     print(sc.to_dict())
 
     sc.power_network.update_shortest_paths()
