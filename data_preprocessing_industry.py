52923515fc15687bd623a0261fc157cccdca5e14
Daniel <daniel.busch@rl-institut.de>
2021-08-19 11:09:00 +0200

    preprocessing industry

--- data_preprocessing_industry.py
+++ data_preprocessing_industry.py
@@ -1,41 +1,62 @@
 import pandas as pd
 import json
 import os
-# TODO! what is the unit of the data set?
+import numpy as np
+
 # input path
 directory = 'C:\\Users\daniel.busch\simply\simply\data\industry'
 filename = 'v_opendata.json'
 path_in = os.path.join(directory, filename)
 
-# output path 
+# output path
 path_out = 'C:\\Users\daniel.busch\simply\simply\data\industry_sample'
 
 # how many days
 days = 7
 
-# timesteps in h
+# timesteps is in h
 # TODO! this should be automatically detected when resampling (use pd.groupby)
 timesteps_h = 0.25
 
 # load industry time series from json file
-with open(path_in,'r') as f:
+with open(path_in, 'r') as f:
     load_curve_industry = json.loads(f.read())
 
-# load names from csv file
+# load names and yearly energy consumption from csv file
 # TODO! use names as file names when saving the csv
-df_names = pd.read_csv(os.path.join(directory, 'data_description.csv'), sep = ';')
+df_ind = pd.read_csv(os.path.join(directory, 'data_description.csv'),
+                     sep=';')
+
+# define energy (electricity) consumption for small and medium entreprises
+df_consumption = pd.DataFrame({'minE': [1e4, 3e4, 1e5, 2.5e5, 1e6, 5e6],
+                               'maxE': [3e4, 1e5, 2.5e5, 1e6, 5e6, 8e6]})
 
 # this dataset uses 60 min timesteps
 nb_ts = 24*365
 time_idx = pd.date_range("2021-01-01", freq="H", periods=nb_ts)
 # save each indsutry sector as csv file
 for i, data in enumerate(load_curve_industry[0]['data']):
-    df = pd.DataFrame({'Time': time_idx, 'load': data['values']}) # create df
-    df = df.set_index('Time') # set index
-    df = df.iloc[0:days*24, 0] # choose time frame
-    rs = df.resample('15min').pad() # resample, in this cas upsample
-    df = rs * timesteps_h # divide by timestep ratio to sustain energy balance
-    industry_type = str(data['internal_id'][0]) # select industry type
-    filename_out = '_'.join([industry_type,'sample.csv'])
+    df = pd.DataFrame({'Time': time_idx, 'load': data['values']})  # create df
+    df = df.set_index('Time')   # set index
+    df = df.iloc[0:days*24, 0]  # choose time frame
+    rs = df.resample('15min').pad()  # resample, in this case upsample
+    df = rs * timesteps_h  # divide by timestep ratio to sustain energy balance
+    industry_type = data['internal_id'][0]  # get industry type
+    # assign a yearly energy consumption to the normalized load
+    if np.isnan(df_ind['yearly consumption [kWh]'].iloc[industry_type-1]):
+        energy_category = np.random.choice(np.arange(0, 6),
+                                           p=[0.29, 0.3, 0.25,
+                                              0.13, 0.02, 0.01])
+        yearly_energy = np.random.randint(df_consumption['minE'].iloc[0],
+                                          df_consumption['maxE'].iloc[0])
+    else:
+        yearly_energy_fixed = df_ind[
+            'yearly consumption [kWh]'].iloc[industry_type-1]
+        yearly_energy = np.random.randint(0.9*yearly_energy_fixed,
+                                          1.1*yearly_energy_fixed)
+    # multiply with yearly energy consumption
+    df = df * yearly_energy
+    # save file
+    filename_out = '_'.join([str(industry_type), 'sample.csv'])
     path_out_complete = os.path.join(path_out, filename_out)
     df.to_csv(path_out_complete, index=True, sep=';')
