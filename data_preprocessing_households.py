d3ed6e60b449726b1f669f2313cad0f717a1aff9
Daniel <daniel.busch@rl-institut.de>
2021-08-25 15:21:18 +0200

    turn scripts into functions with variable input

--- data_preprocessing_households.py
+++ data_preprocessing_households.py
@@ -3,37 +3,41 @@ import os
 import pandas as pd
 import random
 
-# input path
-path_in = 'C:\\Users\daniel.busch\simply\simply\data\households'
 
-# output path 
-path_out = 'C:\\Users\daniel.busch\simply\simply\data\households_sample'
+def preprocess_household_data(dir_in,
+                              dir_out,
+                              n_files,
+                              t_step,
+                              n_days,
+                              t_resample):
 
-# how many files
-n = 10
+    # merge directory path and relative path
+    path_dir = os.path.join(os.getcwd(), dir_in)
 
-# timestep in seconds
-t_step = 60*15 # 15 minutes
+    # read all names from directory
+    filenames = glob.glob(os.path.join(path_dir, '*.csv'))
 
-# how many days
-days = 7
-
-# read all names from directory
-filenames = glob.glob(os.path.join(path_in, '*.csv'))
-
-# df_timesteps = pd.read_csv(filenames[0],
-#                                usecols = ['Time'],
-#                                parse_dates = [0],
-#                                sep = ';')
-
-# read some files and resample and save
-filenames = random.sample(filenames, n)
-for filename in filenames:
-        df = pd.read_csv(filename, sep = ';', nrows = days*24*60, parse_dates = ['Time'], dayfirst = True)
+    # read some files and resample and save
+    filenames = random.sample(filenames, n_files)
+    for filename in filenames:
+        df = pd.read_csv(filename,
+                         sep=';',
+                         nrows=n_days*24*60,
+                         parse_dates=['Time'],
+                         dayfirst=True)
         df = df.set_index('Time')
         df = df.drop(columns='Electricity.Timestep')
-        df = df.resample('15min').sum()
-        filename_out = '_'.join([os.path.basename(filename)[:-4],'sample.csv'])
-        path_out_complete = os.path.join(path_out, filename_out)
+        df = df.resample(t_resample).sum()
+        filename_out = '_'.join([os.path.basename(filename)[:-4],
+                                'sample.csv'])
+        path_out_complete = os.path.join(dir_out, filename_out)
         df.to_csv(path_out_complete, index=True, sep=';')
-        
+
+
+if __name__ == "__main__":
+    preprocess_household_data(dir_in=r'simply\data\households',
+                              dir_out=r'simply\data\households_sample',
+                              n_files=10,
+                              t_step=60*15,
+                              n_days=7,
+                              t_resample="15min")
--- data_preprocessing_industry.py
+++ data_preprocessing_industry.py
@@ -3,66 +3,87 @@ import json
 import os
 import numpy as np
 
-# input path
-directory = 'C:\\Users\daniel.busch\simply\simply\data\industry'
-filename = 'v_opendata.json'
-path_in = os.path.join(directory, filename)
+def preprocess_industry_data(dir_in,  # directory of original data
+                             filename,  # filename
+                             t_step,  # time step in hours (must match t_resample)
+                             n_days,  # how many days
+                             t_resample):  # '15min', '2min' etc.
+# TODO! until now this function can only upsample not downsample the data
+# TODO! t_step and t_resample are separate variables even though they contain
+# the same information
 
-# output path
-path_out = 'C:\\Users\daniel.busch\simply\simply\data\industry_sample'
+    # merge directory path and relative path
+    path_dir = os.path.join(os.getcwd(), dir_in)  # is this necessary?
+    path_in = os.path.join(path_dir, filename)
 
-# how many days
-days = 7
+    # load industry time series from json file
+    with open(path_in, 'r') as f:
+        load_curve_industry = json.loads(f.read())
 
-# timesteps is in h
-# TODO! this should be automatically detected when resampling (use pd.groupby)
-timesteps_h = 0.25
+    # this dataset uses 60 min timesteps
+    nb_ts = 24*365
+    time_idx = pd.date_range("2021-01-01", freq="H", periods=nb_ts)
+    # save each indsutry sector as df
+    df_list = []
+    for i, data in enumerate(load_curve_industry[0]['data']):
+        industry_type = data['internal_id'][0]  # get industry type
+        df = pd.DataFrame(
+            {'Time': time_idx,
+             str(industry_type): data['values']
+             }
+            )  # create df
+        df = df.set_index('Time')   # set index
+        df = df.iloc[0:n_days*24, 0]  # choose time frame
+        rs = df.resample(t_resample).pad()  # resample, in this case upsample
+        df = rs * t_step  # divide by timestep ratio to sustain energy balance
 
-# load industry time series from json file
-with open(path_in, 'r') as f:
-    load_curve_industry = json.loads(f.read())
+        df_list.append(df)
+    # return as list of dataframes
+    return df_list
 
-# load names and yearly energy consumption from csv file
-# TODO! use names as file names when saving the csv
-df_ind = pd.read_csv(os.path.join(directory, 'data_description.csv'),
-                     sep=';')
+def scale_industry_data(df_list, directory, categories, probabilities):
+    df_list_scale = []
+    for df in df_list:
+        # choose a category based on the set probabilties
+        energy_category = np.random.choice(np.arange(
+            0, len(probabilities)), p=probabilities)
+        # randomly choose a value in the given range
+        yearly_energy = np.random.randint(categories['minE'].iloc[energy_category],
+                                          categories['maxE'].iloc[energy_category])
+        # multiply with yearly energy consumption
+        df = df * yearly_energy
+        # append to list
+        df_list_scale.append(df)
+    # return as list of dataframes
+    return df_list_scale
 
-# define energy (electricity) consumption for small and medium entreprises
-df_consumption = pd.DataFrame({'minE': [1e4, 3e4, 1e5, 2.5e5, 1e6, 5e6],
-                               'maxE': [3e4, 1e5, 2.5e5, 1e6, 5e6, 8e6]})
+def save_industry_data(df_list, dir_out):
+    for df in df_list:
+        # generate filename based on industry type
+        filename_out = '_'.join([str(df.name), 'sample.csv'])
+        # merge directory and filename to path
+        path_out_complete = os.path.join(dir_out, filename_out)
+        #  save as csv
+        df.to_csv(path_out_complete, index=True, sep=';')
 
-# this dataset uses 60 min timesteps
-nb_ts = 24*365
-time_idx = pd.date_range("2021-01-01", freq="H", periods=nb_ts)
-# save each indsutry sector as csv file
-for i, data in enumerate(load_curve_industry[0]['data']):
-    df = pd.DataFrame({'Time': time_idx, 'load': data['values']})  # create df
-    df = df.set_index('Time')   # set index
-    df = df.iloc[0:days*24, 0]  # choose time frame
-    rs = df.resample('15min').pad()  # resample, in this case upsample
-    df = rs * timesteps_h  # divide by timestep ratio to sustain energy balance
-    industry_type = data['internal_id'][0]  # get industry type
-    # assign a yearly energy consumption to the normalized load
-    # the data is taken from the csv file data_description.csv
-    # some industrial companies can have very differnet sizes
-    # these get a yearly energy consumption from this study:
-    # https://www.eoptimum.de/energie-news/vom-wollen-und-koennen-rund-um-energieeinsparungen-in-unternehmen-12/ 
-    if np.isnan(df_ind['yearly consumption [kWh]'].iloc[industry_type-1]):
-        energy_category = np.random.choice(np.arange(0, 6),
-                                           p=[0.29, 0.3, 0.25,
-                                              0.13, 0.02, 0.01])
-        yearly_energy = np.random.randint(df_consumption['minE'].iloc[0],
-                                          df_consumption['maxE'].iloc[0])
-    # some industrial companies usually need a lot of energy
-    # they are assigned a value from research +- 10%
-    else:
-        yearly_energy_fixed = df_ind[
-            'yearly consumption [kWh]'].iloc[industry_type-1]
-        yearly_energy = np.random.randint(0.9*yearly_energy_fixed,
-                                          1.1*yearly_energy_fixed)
-    # multiply with yearly energy consumption
-    df = df * yearly_energy
-    # save file
-    filename_out = '_'.join([str(industry_type), 'sample.csv'])
-    path_out_complete = os.path.join(path_out, filename_out)
-    df.to_csv(path_out_complete, index=True, sep=';')
+if __name__ == "__main__":
+    # resample industry data
+    industry_data_sample = preprocess_industry_data(
+        dir_in=r'simply\data\industry',
+        filename = 'v_opendata.json',
+        t_step=0.25,
+        n_days=7,
+        t_resample="15min")
+    # scale industry data
+    industry_data_sample = scale_industry_data(
+        industry_data_sample,
+        directory= r'simply\data\industry',
+        categories=pd.DataFrame(
+                  {'minE': [1e4, 3e4, 1e5, 2.5e5, 1e6, 5e6],
+                   'maxE': [3e4, 1e5, 2.5e5, 1e6, 5e6, 8e6]
+                  }
+                               ),
+        probabilities=[0.29, 0.3, 0.25, 0.13, 0.02, 0.01])
+    # save industry data
+    save_industry_data(df_list=industry_data_sample,
+        dir_out=r'simply\data\industry_sample')
