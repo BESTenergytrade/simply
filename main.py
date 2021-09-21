81dc8d78badeedbd1590033708d7ee61d01bff49
Justin Hodura <Justin.Hodura@rl-institut.de>
2021-09-21 14:26:44 +0200

    docu main, describing the geńeral functions

--- main.py
+++ main.py
@@ -21,7 +21,21 @@ class Config:
         self.path = Path('./scenarios/default')
 
 
+
 if __name__ == "__main__":
+    """ defining and importing the objects "cfg" for the config class and "sc" for the scenario class.
+    
+    the plots are only plotted in "if show_plots" when the flag over the script is show_plots=true. With the current setup the first if-function is ignored.
+    
+    the variable "a" represents the different actors and is imported through the class "actor" and with the instance/object "sc.actors". The variable "a.t" represents the starting point of the "config" class
+    
+    the columns "load", "pv" and "prices" of each actor is loaded into the script per timestamp.
+    
+    bids and asks are loaded into the script and orders are generated. order_df sums up the bids, asks, actor_id and the price per timestamp from 8-11 (after each loop)
+    
+    werden durch sc.map_actors die Prosumenten den jeweiligen Knotenpunkten zugewiesen?
+    was genau ist mit callback gemeint?
+    """
     cfg = Config()
     sc = scenario.create_random(12, 10)
     # TODO make output folder for config file and Scenario json files, output series in csv and plots files
