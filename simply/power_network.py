2fae3a57efff38632ed5b07dd679125cf85c6da3
Justin Hodura <Justin.Hodura@rl-institut.de>
2021-09-29 14:26:22 +0200

    update with FR format power_network

--- simply/power_network.py
+++ simply/power_network.py
@@ -1,5 +1,8 @@
-"""the script powernetwork.py uses the library networkx to create a random network with nodes and edges. Important about this script is the definition of weights on the edges.
-Through the weights the different net fees are taken into account and are a key component of the simulation in general.
+"""
+JuH: the script powernetwork.py uses the library networkx to create a random network/grid with nodes and edges.
+Important about this script is the definition of weights of the different edges.
+Through the weights the different net fees are taken into account which are a key component of the simulation in general.
+
 """
 
 import json
@@ -11,22 +14,11 @@ import matplotlib.pyplot as plt
 
 class PowerNetwork:
     def __init__(self, name, network):
-        """ Class PowerNetwork is the representation of a grid
-
-        the powernetwork is designed to update and find out the shortest path in the grid. The participants are then added randomly to the nodes. The participants are then mapped onto the nodes.
-        The nodes are weighted differently and should represent the different network charges.
-
-        :param name:
-        :param network:
-
-        with networkx the short path is being updated and calculates the shortest path between the participants
-
-        :def add_actors_random: list is created with base_id of the nodes, list goes as long as many participants are involved
-
-        :connections = random.choices: returns a list of random selected elements of the sequence "self.leaf_node" and has the length of the active participants
-
-         In the beginning a empty dictionary is created called map, then the last value of list "actor_nodes" is assigned to variable a, then the last value of list connections is assigned to variable c. In map[a]=c command inside the map dictionary a new key with the name variable "a"is created and the value of that key is assigned as variable "c"
-
+        """
+        Parameters
+        ----------
+        name: string
+        network:
         """
         self.name = name
         self.network = network
@@ -35,6 +27,9 @@ class PowerNetwork:
         self.short_paths = {}
 
     def update_shortest_paths(self):
+        """
+        JuH: identifies the shortest paths in the grid and collects them in a dictionary.
+        """
         self.short_paths = dict(nx.all_pairs_shortest_path(self.network))
 
     def to_image(self, width, height):
@@ -53,6 +48,9 @@ class PowerNetwork:
         return fig
 
     def to_dict(self):
+        """
+        JuH:
+        Returns graph of the node-link data"""
         return json_graph.node_link_data(self.network)
 
     def to_json(self):
@@ -61,6 +59,15 @@ class PowerNetwork:
         json.dump(net_json, open(filename, "w"), indent=2)
 
     def add_actors_random(self, actors):
+        """
+        JuH: Actors are added randomly to nodes. Under connections list is created with the length of the number of actors.
+        empty dictionary is created called map. last value of list "actor_nodes" is assigned to variable a, then the last value of list connections is assigned to variable c.
+        In map[a]=c command, a new key with the name variable "a" is created inside the map dictionary and the value of that key is assigned as variable "c".
+
+        Parameters
+        ----------
+        actors: integer
+        """
         base_id = max(self.network.nodes()) + 100
         num_actors = len(actors)
         actor_nodes = list([*range(base_id, base_id + len(actors), 1)])
@@ -76,6 +83,12 @@ class PowerNetwork:
         return map
 
     def add_actors_map(self, map):
+        """
+
+        Parameters
+        ----------
+        map:
+        """
         base_id = max(self.network.nodes()) + 100
         for a, c in map.items():
             self.network.add_edge(c, a + base_id)
@@ -85,7 +98,7 @@ class PowerNetwork:
 
 def create_random(nodes):
     """
-    edges can be 0 or 1 and describe the number of adjecent of each variable e. The 12 nodes are loaded in with the adjecent numbers. One after the other the weight is added.
+    JuH: edges can be 0 or 1 and describe the number of adjecent of each variable e. The 12 nodes are loaded in with the adjecent numbers. One after the other the weight is added.
     Parameters
     ----------
     nodes
@@ -106,14 +119,10 @@ def create_random(nodes):
 
 def create_random2(nodes):
     """
-
+    JuH: nodes are sorted ??
     Parameters
     ----------
     nodes
-
-    Returns
-    -------
-
     """
     # TODO get number of leaves as parameter
     nw = nx.random_tree(nodes)
