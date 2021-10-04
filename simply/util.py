0c92aa4f640a6116325c3344dd378375e3bb21bc
Justin Hodura <Justin.Hodura@rl-institut.de>
2021-10-04 10:36:30 +0200

    aktualisiert

--- simply/util.py
+++ simply/util.py
@@ -1,8 +1,24 @@
+"""
+JuH: The script "util-py" defines a normal distribution for the pv generation.
+"""
+
 import numpy as np
 import pandas as pd
 
 
 def gaussian_pv(x, mean, std):
+    """
+    JuH: preparation for the pv generation in form of a shifted normal distribution
+    Parameters
+    ----------
+    x
+    mean
+    std
+
+    Returns
+    -------
+
+    """
     return np.exp(-((x - mean) ** 2) / (2 * std ** 2))
 
 
