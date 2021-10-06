b7345a6883a6091d001e50e58c3150218cb8d2ca
Justin Hodura <Justin.Hodura@rl-institut.de>
2021-10-06 11:45:53 +0200

    incorporation of Stefans improvements

--- simply/util.py
+++ simply/util.py
@@ -1,6 +1,6 @@
 """
-JuH: The script "util-py" defines a normal distribution for the pv generation.
-"""
+JuH: The script "util.py" defines a normal distribution for the pv generation.
+bucket for helper functions that are not really tied to the market."""
 
 import numpy as np
 import pandas as pd
@@ -9,20 +9,23 @@ import pandas as pd
 def gaussian_pv(x, mean, std):
     """
     JuH: preparation for the pv generation in form of a shifted normal distribution
+    Returns
+
     Parameters
     ----------
     x
     mean
     std
-
-    Returns
     -------
-
+    Returns: value on gaussian bell curve for a given mean and standard deviation.
+        factors missing?
     """
     return np.exp(-((x - mean) ** 2) / (2 * std ** 2))
 
 
 def summerize_actor_trading(sc):
+    """StS: returns all trades for a given scenario.
+    why is it not a member function of Scenario?"""
     return (
         pd.DataFrame.from_dict([a.traded for a in sc.actors])
         .unstack()
