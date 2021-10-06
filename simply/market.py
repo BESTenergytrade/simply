5b37e38259d81bc7f626ab4ea5e93b0cdd38ba02
Justin Hodura <Justin.Hodura@rl-institut.de>
2021-10-06 11:39:49 +0200

    incorporation of Stefans improvements

--- simply/market.py
+++ simply/market.py
@@ -1,7 +1,8 @@
 """
-JuH: In this file lists are created which summarize the different bids and asks depending on the type (1 for bids, -1 for asks).
+StS: implements a generic Market, which keeps track of bids and asks and also implements a basic matching strategy.
+Intended as a parent class for more sophisticated trading strategies.
+JuH: In this file lists are created which summarize the different bids and asks depending on the type(1: bids, -1: asks)
 Bids and asks are implemented into the order list.
-A dictionary is created for the matches and a list is created for the trades.
 """
 
 import pandas as pd
@@ -12,11 +13,9 @@ class Market:
     def __init__(self, time):
 
         """
-        JuH:creation of empty lists and dictionaries
-
         Parameters
         ----------
-        time
+        time: current timestep
         """
 
         # TODO tbd if lists or dicts or ... is used
@@ -31,31 +30,38 @@ class Market:
         self.actor_callback = {}
 
     def get_bids(self):
+        """StS: helper function, returns all bids from the order dataframe as a pandas dataframe"""
         self.get_order_df()
         return self.order_df[self.order_df["type"] == 1]
 
     def get_asks(self):
+        """StS: helper function, returns all asks from the order dataframe as a pandas dataframe"""
         self.get_order_df()
         return self.order_df[self.order_df["type"] == -1]
 
     def get_order_df(self):
+        """StS: translates all saved orders into pandas dataframe. Updates order_df property."""
         self.order_df = pd.DataFrame(self.orders)
         return self.order_df
 
     def get_all_matches(self):
+        """StS: gets all matches (dictionary bid_id -> ask_id)"""
         return self.matches
 
     def print(self):
+        """print: writes bids and asks to console"""
         print(pd.DataFrame(self.bids))
         print(pd.DataFrame(self.asks))
 
     def accept_order(self, order, callback):
         """
-        JuH: the order type is checked and sorted into asks and bids
+        JuH: the order type is checked (must be +1 (ask) or -1 (bid)) and sorted into asks and bids.
+        StS: Order.time must be the same as the market time
+        (may be subject to change, as unmatched orders can be left in the market).
         Parameters
         ----------
-        order
-        callback
+        order: actor.Order
+        callback: function to call when order is matched (signature: time, type, energy, price)
         """
         assert order.time == self.t
         self.orders.append(order)
@@ -69,7 +75,9 @@ class Market:
 
     def clear(self):
         """
-        JuH: creates an empty list with the key actor_id. The list is then filled with all actors and the type (bid, ask) and the price.
+        StS: clear market -> match orders, process matches. No return value.
+        JuH: creates an empty list with the key actor_id.
+        The list is then filled with all actors and the type (bid, ask) and the price.
         """
         # TODO match bids
         self.matches = self.match()
@@ -89,6 +97,10 @@ class Market:
                 ac(self.t, a_trades["type"].iloc[0], energy, price)
 
     def match(self, show=False):
+        """match: generic market matching strategy
+        Returns
+        -------
+        object: matches"""
         # TODO default match can be replaced in different subclass
         orders = self.get_order_df()
         # Expand bids/asks to fixed energy quantity bids/asks with individual order ids
