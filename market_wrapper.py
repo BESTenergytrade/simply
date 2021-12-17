5bc93e6aa0610c4dd27262c44ba502369c9cbae7
stefan.schirmeister <stefan.schirmeister@localiser.de>
2021-12-17 14:56:35 +0100

    market wrapper: extract functions

--- market_wrapper.py
+++ market_wrapper.py
@@ -12,40 +12,49 @@ Config('')
 ENERGY_UNIT_CONVERSION_FACTOR = 1000  # simply: kW, D3A: MW
 
 
+def accept_orders(market, orders):
+    for bid in orders["bids"]:
+        order = Order(-1, bid["time_slot"], bid["id"], bid["energy"] * ENERGY_UNIT_CONVERSION_FACTOR, bid["energy_rate"])
+        market.accept_order(order, None)
+    for ask in orders["offers"]:
+        order = Order(1, ask["time_slot"], ask["id"], ask["energy"] * ENERGY_UNIT_CONVERSION_FACTOR, ask["energy_rate"])
+        market.accept_order(order, None)
+
+
+def generate_recommendations(market_id, time, bids, asks, matches):
+    recommendations = []
+
+    for match in matches:
+        recommendations.append({
+            "market_id": market_id,
+            "time_slot": time,
+            "bids": bids[match["bid_actor"]],
+            "offers": asks[match["ask_actor"]],
+            "selected_energy": match["energy"] / ENERGY_UNIT_CONVERSION_FACTOR,
+            "trade_rate": match["price"],
+        })
+
+    return recommendations
+
+
 class PayAsBidMatchingAlgorithm():
 
     def get_matches_recommendations(mycoDict):
 
-        recommendation = []
+        recommendations = []
 
         for market_id, market in mycoDict.items():
             for time, orders in market.items():
                 m = Market(time = time + ":00")
-                bids = {}
-                asks = {}
+                bids = {bid["id"]: bid for bid in orders["bids"]}
+                asks = {ask["id"]: ask for ask in orders["offers"]}
 
-                for bid in orders["bids"]:
-                    order = Order(-1, bid["time_slot"], bid["id"], bid["energy"] * ENERGY_UNIT_CONVERSION_FACTOR, bid["energy_rate"])
-                    m.accept_order(order, None)
-                    bids[bid["id"]] = bid
-                for ask in orders["offers"]:
-                    order = Order(1, ask["time_slot"], ask["id"], ask["energy"] * ENERGY_UNIT_CONVERSION_FACTOR, ask["energy_rate"])
-                    m.accept_order(order, None)
-                    asks[ask["id"]] = ask
-
+                accept_orders(m, orders)
                 matches = m.match()
 
-                for match in matches:
-                    recommendation.append({
-                        "market_id": market_id,
-                        "time_slot": time,
-                        "bids": bids[match["bid_actor"]],
-                        "offers": asks[match["ask_actor"]],
-                        "selected_energy": match["energy"] / ENERGY_UNIT_CONVERSION_FACTOR,
-                        "trade_rate": match["price"],
-                    })
+                recommendations += generate_recommendations(market_id, time, bids, asks, matches)
 
-        return recommendation
+        return recommendations
 
 
 if __name__ == "__main__":
