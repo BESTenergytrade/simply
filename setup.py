750d72dc32f84846ea004ce0c943fa37e8ed0be8
jtm91 <jtm91@mailbox.tu-berlin.de>
2022-01-10 15:40:53 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+try:
+    with open("requirements.txt") as simply_req:
+        REQUIREMENTS = [].append(simply_req)
+
+except OSError:
+    # Shouldn't happen
+    REQUIREMENTS = []
+
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    README = readme.read()
+
+VERSION = 0.1
+
+setup(
+    name="simply",
+    description="Simulation of Electricity Markets in Python",
+    long_description=README,
+    url="https://github.com/BESTenergytrade/simply.git",
+    version=VERSION,
+    packages=find_packages(where=".", exclude=["tests"]),
+    package_dir={"simply": "simply"},
+    package_data={},
+    install_requires=REQUIREMENTS,
+    zip_safe=False,
+)
