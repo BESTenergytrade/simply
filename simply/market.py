340b492a82dbe451e0a936c29f661076085c2567
stefan.schirmeister <stefan.schirmeister@localiser.de>
2022-01-20 13:57:02 +0100

    fix #54

--- simply/market.py
+++ simply/market.py
@@ -14,6 +14,7 @@ class Market:
         self.energy_unit = cfg.parser.getfloat("market", "energy_unit", fallback=0.1)
         self.actor_callback = {}
         self.network = network
+        self.EPS = 1e-10
 
     def get_bids(self):
         return self.orders[self.orders["type"] == -1]
@@ -36,10 +37,11 @@ class Market:
         if order.type not in [-1, 1]:
             raise ValueError("Wrong order type ({})".format(order.type))
         # make certain energy has step size of energy_unit
-        energy = (order.energy // self.energy_unit) * self.energy_unit
+        energy = ((order.energy + self.EPS) // self.energy_unit) * self.energy_unit
         # make certain enough energy is traded
         if energy < self.energy_unit:
             return
+        order = order._replace(energy=energy)
         self.orders = self.orders.append(pd.DataFrame([order]), ignore_index=True)
         self.actor_callback[order.actor_id] = callback
 
