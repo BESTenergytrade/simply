db3a3c5b281489a308767cb5a65a90d5d6995f81
jtm91 <jtm91@mailbox.tu-berlin.de>
2022-01-24 19:10:51 +0100

    Add fair clustered market wrapper class

--- market_wrapper.py
+++ market_wrapper.py
@@ -78,6 +78,35 @@ class PayAsClearMatchingAlgorithm():
         return recommendations
 
 
+class ClusterPayAsClearMatchingAlgorithm():
+
+    def get_matches_recommendations(mycoDict):
+        pn = power_network.create_random(1)
+        recommendations = []
+
+        for market_id, market_name in mycoDict.items():
+            for time, orders in market_name.items():
+                actors = [bid["id"] for bid in orders["bids"]] + \
+                         [ask["id"] for ask in orders["offers"]]
+                # Give actors a position in the network
+                # Currently at a single node with id 0
+                actor_nodes = [0 for i in actors]
+                map_actors = {actor: node_id for actor, node_id in zip(actors, actor_nodes)}
+                pn.add_actors_map(map_actors)
+
+                m = market_fair.BestMarket(t=time + ":00", network=pn)
+                bids = {bid["id"]: bid for bid in orders["bids"]}
+                asks = {ask["id"]: ask for ask in orders["offers"]}
+
+                accept_orders(m, orders)
+                matches = m.match()
+
+                recommendations += generate_recommendations(market_id, time, bids, asks,
+                                                            matches)
+
+        return recommendations
+
+
 if __name__ == "__main__":
     parser = ArgumentParser(description='Wrapper for myco API')
     parser.add_argument('file', help='myco API data file')
@@ -87,5 +116,6 @@ if __name__ == "__main__":
         mycoDict = json.load(f)
 
     # recommendation = PayAsBidMatchingAlgorithm.get_matches_recommendations(mycoDict)
-    recommendation = PayAsClearMatchingAlgorithm.get_matches_recommendations(mycoDict)
+    # recommendation = PayAsClearMatchingAlgorithm.get_matches_recommendations(mycoDict)
+    recommendation = ClusterPayAsClearMatchingAlgorithm.get_matches_recommendations(mycoDict)
     print(json.dumps(recommendation, indent=2))
