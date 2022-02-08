73ae5aac93a6615140d9bc8ff04110d3673cffba
jtm91 <jtm91@mailbox.tu-berlin.de>
2022-02-08 16:40:57 +0100

    Isolate test for price ordering from existing test

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -59,6 +59,50 @@ class TestPayAsBidMatchingAlgorithm:
         assert recommendations == expected_recommendations
 
     @staticmethod
+    def test_perform_pay_as_bid_match_multiple_offers_bids_price_ordered():
+        """
+        Test whether the matches from a list of offers and bids are the expected ones.
+        The orders are matched in the order of decreasing and increasing energy_rate
+        for bids and asks respectively.
+        """
+        data = {
+            "market1": {
+                "2021-10-06T12:00": {
+                    # The bid matching order should be 3, 2, 1 (decreasing energy rate) and the
+                    # order matching offer should be 4, 6, 5 (increasing energy rate).
+                    "bids": [
+                        {"id": 1, "buyer": "A", "energy_rate": 1, "energy": 10},
+                        {"id": 2, "buyer": "B", "energy_rate": 2, "energy": 15},
+                        {"id": 3, "buyer": "C", "energy_rate": 3, "energy": 20},
+                    ],
+                    "offers": [
+                        {"id": 4, "seller": "A", "energy_rate": 1 + FLOATING_POINT_TOLERANCE,
+                         "energy": 25},
+                        {"id": 5, "seller": "B", "energy_rate": 5, "energy": 30},
+                        {"id": 6, "seller": "C", "energy_rate": 2.4, "energy": 35},
+                    ],
+                }
+
+            }
+        }
+        recommendations = PayAsBidMatchingAlgorithm.get_matches_recommendations(data)
+        expected_recommendations = [
+            {"market_id": "market1",
+             "time_slot": "2021-10-06T12:00",
+             "bid": {"id": 3, "buyer": "C", "energy_rate": 3, "energy": 20},
+             "offer": {"id": 4, "seller": "A", "energy_rate": 1.00001, "energy": 25},
+             "selected_energy": 20, "trade_rate": 3, "matching_requirements": None},
+
+            {"market_id": "market1",
+             "time_slot": "2021-10-06T12:00",
+             "bid": {"id": 2, "buyer": "B", "energy_rate": 2, "energy": 15},
+             "offer": {"id": 4, "seller": "A", "energy_rate": 1.00001, "energy": 25},
+             "selected_energy": 5, "trade_rate": 2, "matching_requirements": None},
+        ]
+        # ordering of matches within recommendation list is not important
+        assert [i for i in recommendations if i not in expected_recommendations] == []
+
+    @staticmethod
     def test_perform_pay_as_bid_match_multiple_offers_bids():
         """
         Test whether the matches from a list of offers and bids are the expected ones.
