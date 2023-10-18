d7bd9e7355363f5cca8a1344a32912156182895a
jtm91 <jtm91@mailbox.tu-berlin.de>
2023-10-18 10:29:22 +0200

    Remove file

--- ChatGPT_test.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-
-def main(project_dir, data_dir):
-    print('project_dir:', project_dir)
-    # File path for the matches csv
-    project_dir = Path(project_dir)
-    matches_fp = project_dir / 'market_results' / 'matches.csv'
-    print(f'matches_fp: {matches_fp}')
