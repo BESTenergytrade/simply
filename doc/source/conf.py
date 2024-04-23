63b162a60cf117e469644c895b0dab79eb3d83f3
Bedirhan Köse <41688971+bedirhankose@users.noreply.github.com>
2024-04-23 13:42:54 +0200

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -57,3 +57,10 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+warnings = {
+    'ignore': [
+        "No Configuration file path was provided.",
+        "No project_dir was provided."
+    ],
+}
