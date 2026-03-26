2ad4cf51b1e14c8cca245202a54e35d88a675e0c
j-ti <56872869+j-ti@users.noreply.github.com>
2026-03-26 08:00:49 +0100

    Guarantee Time column

--- simply/actor.py
+++ simply/actor.py
@@ -1108,8 +1108,8 @@ class Actor:
         #  also errors need to be saved.
         if self.error_scale != 0:
             raise Exception('Prediction Error is not yet implemented!')
-        save_df = self.data[["load", "pv", "schedule"]].reset_index()
-        save_df["Time"] = save_df["Time"].dt.strftime('%Y-%m-%d %H:%M:%S')
+        save_df = self.data[["load", "pv", "schedule"]].copy()
+        save_df.index.name = "Time"
         if self.var_battery.capacity > 0:
             save_df.loc[:, ["ev_avail", "ev_demand"]] = self.data[["ev_avail", "ev_demand"]]
         save_df.to_csv(dirpath.joinpath(self.csv_file))
